9dbfcdc5b16fb5586941824d88487a906d2fa2dd
ashantisease19 <77317067+ashantisease19@users.noreply.github.com>
2022-10-06 15:35:21 -0400

    updated everything

--- projectpab/Assets/Script/PlayerControl.cs
+++ projectpab/Assets/Script/PlayerControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public Transform otherObject;
-    public float speed = 5;
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +20,18 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxis("Horizontal");
-        if (Input.GetKey(KeyCode.D))
+        //float xMove = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.A))
         {
-            speed = -5;
+            speed = -10;
         } else if (Input.GetKey(KeyCode.D))
         {
-            speed = 5;
+            speed = 10;
         } else
         {
             speed = 0;
         }
-        transform.Translate(xMove * speed * Time.deltaTime, 0, 0);
+        transform.Translate(speed * Time.deltaTime, 0, 0);
     }
 }
