89ebd8643a6faa3728bd714f1ae6f8a45d4e1885
ashantisease19 <77317067+ashantisease19@users.noreply.github.com>
2022-10-06 17:22:56 -0400

    vs has update

---
+++ projectpab/Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject trashPrefab;
+    public Transform leftTran;
+    public Transform rightTran;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    InvokeRepeating("SpawnItem", 0, 2);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpawnItem()
+    {
+       float rndXValue = Random.Range(leftTran.position.x, rightTran.position.x);
+
+       Vector2 spawnPos = new Vector2(rndXValue, leftTran.position.y);
+
+       Instantiate(trashPrefab, spawnPos, Quaternion.identity );
+    }
+}
+
--- projectpab/Assets/Script/PlayerControl.cs
+++ projectpab/Assets/Script/PlayerControl.cs
@@ -4,34 +4,46 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public Transform otherObject;
-    private float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(transform.position.x);
-       
-            
+	public Transform otherObject;
+	private float speed;
+	// Start is called before the first frame update
+	void Start()
+	{
+		Debug.Log(transform.position.x);
+	   
+			
 
 
-    }
+	}
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        //float xMove = Input.GetAxis("Horizontal");
+	// Update is called once per frame
+	void Update()
+	{
+		//float xMove = Input.GetAxis("Horizontal");
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            speed = -10;
-        } else if (Input.GetKey(KeyCode.D))
-        {
-            speed = 10;
-        } else
-        {
-            speed = 0;
-        }
-        transform.Translate(speed * Time.deltaTime, 0, 0);
-    }
+		if (Input.GetKey(KeyCode.A))
+		{
+			speed = -10;
+		} else if (Input.GetKey(KeyCode.D))
+		{
+			speed = 10;
+		} else
+		{
+			speed = 0;
+		}
+		transform.Translate(speed * Time.deltaTime, 0, 0);
+	}
+	private void OnTriggerEnter2D(Collider 2D collision)
+	{
+		if (collision.gameObject.tag -- "Coin" 
+		{
+			Destroy(collision.gameObject);
+		}
+
+		if (collision.gameObject.tag -- "Hazard") 
+		{
+			Destroy(gameObject);
+		}
+	}
 }
